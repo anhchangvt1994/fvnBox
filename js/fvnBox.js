f2383190c0c1d5f871ccff7bf26d0ecbee00b047
anhchangvt1994 <anhchangvt1994@users.noreply.github.com>
2018-01-08 17:27:35 +0700

    changed

--- js/fvnBox.js
+++ js/fvnBox.js
@@ -281,6 +281,7 @@ $(function($) {
         if (!storage.outImg) {
           var leftImgPos, leftImgBox;
           var curPoint = storage.event.originalEvent.touches[0].pageX;
+          console.log("curPoint :"+curPoint);          
           var left;
           if ($($(".fullImg").find("img"))[1] === undefined) {
             $($(".fullImg").find("img")).removeClass("returnAnimate quickMove").addClass("noneAnimate");
@@ -291,11 +292,13 @@ $(function($) {
           }
           leftImgPos = $(".fullImg img").offset().left;
           leftImgBox = $(".fullImg .imgBox").offset().left;
+          console.log("leftImgBox :"+leftImgBox);
+          console.log("storage.prevPoint :"+storage.prevPoint);
           if (curPoint > storage.prevPoint) {
             leftImgPos = leftImgPos + 5;
             leftImgBox = leftImgBox + 5;
             storage.distance = storage.distance + 1;
-          } else {
+          } else if(curPoint < storage.prevPoint) {
             leftImgPos = leftImgPos - 5;
             leftImgBox = leftImgBox - 5;
             storage.distance = storage.distance - 1;
@@ -466,23 +469,23 @@ $(function($) {
       actualHeight = (imgSize.actualHeight !== undefined ? parseInt(imgSize.actualHeight) : undefined);       
       var increaseSize,navWidth,overflow_cord;      
       if(fvnBoxFeature.detectDevice()){
-        increaseSize = 10;
+        increaseSize = 70;
         navWidth = 1;        
         overflow_cord = "auto";
       }else{
-        increaseSize = 70;
+        increaseSize = 0;
         overflow_cord = "hidden";
       }      
       var scrollBox,scrollContent,imgBox,navBox,fvnInforBox,commonSize;        
       if(actualWidth!==undefined && actualHeight === undefined){
-        scrollBox = {"width":actualWidth,"height":trueH+increaseSize,"z-index":"99999"};
+        scrollBox = {"width":actualWidth,"height":trueH+70,"z-index":(fvnBoxFeature.detectDevice() === true ? "" : "99999")};
         scrollContent = {"overflow-x":overflow_cord,"overflow-y":"hidden"};
         imgBox = {"width":actualWidth+10,"height":trueH+10};
         navBox = {"width":actualWidth * (navWidth !== undefined ? navWidth : 1.25),"height":trueH};
-        fvnInforBox = {"width":actualWidth,"height":trueH+increaseSize};
+        fvnInforBox = {"width":actualWidth,"height":trueH+70};
       }else if(actualWidth===undefined && actualHeight !== undefined){
-        scrollBox = {"width":trueW + increaseSize,"height":actualHeight,"z-index":"99999"};
-        scrollContent = {"overflow-x":"hidden","overflow-y":overflow_cord,"padding-right":(fvnBoxFeature.detectDevice() == true ? "" : "17px")};
+        scrollBox = {"width":trueW + 70,"height":actualHeight,"z-index":(fvnBoxFeature.detectDevice() === true ? "" : "99999")};
+        scrollContent = {"overflow-x":"hidden","overflow-y":overflow_cord,"padding-right":(fvnBoxFeature.detectDevice() === true ? "" : "17px")};
         imgBox = {"width":trueW + 10,"height":actualHeight + 10};
         navBox = {"width":trueW * (navWidth !== undefined ? navWidth : 1.45),"height":actualHeight};
         fvnInforBox = {"width":trueW,"height":actualHeight};
@@ -665,6 +668,9 @@ $(function($) {
       }
 
       function startDrag(ev){
+        if(ev.originalEvent !== undefined && fvnBoxFeature.detectDevice()){
+          ev = ev.originalEvent.touches[0];          
+        }
         if(cordinate == "x"){
           rootPos = ev.pageX;
           contentPos = $(wrapperContent).scrollLeft();          
@@ -675,13 +681,17 @@ $(function($) {
         beginDrag = true;
       }
       
-      function scrollBar(ev){        
+      function scrollBar(ev){  
+        if(ev.originalEvent !== undefined && fvnBoxFeature.detectDevice()){
+          ev = ev.originalEvent.touches[0];          
+        }
         if(beginDrag){
           if(cordinate == "x"){
             let mouseDifferential = ev.pageX - rootPos;
             let scrollEquivalent = mouseDifferential * (wrapperSize / scrollSize);
             $(wrapperContent).scrollLeft(contentPos + scrollEquivalent);            
           }else{
+            console.log(ev.pageY);
             let mouseDifferential = ev.pageY - rootPos;
             let scrollEquivalent = mouseDifferential * (wrapperSize / scrollSize);
             $(wrapperContent).scrollTop(contentPos + scrollEquivalent);
@@ -705,15 +715,25 @@ $(function($) {
             $(scroller).css({"width":15+"px","height":scrollerCordinate+"px","top":($(wrapperContent).scrollTop()/wrapperSize)*(scrollSize - 5)+"px","right":2+"px"});
             scrollContent.addClass('fvnShowY');
           }                    
-          scroller.addEventListener('mousedown',startDrag);
-          if(fvnBoxFeature.detectDevice()){
-            scrollContent.addClass('fvnHideCord');
-          }else{            
-            scrollContent.removeClass('fvnHideCord');
-            scrollContent.append(scroller);      
-          }
-          window.addEventListener('mouseup',stopDrag);
-          window.addEventListener('mousemove',scrollBar);
+          // scroller.addEventListener('mousedown',startDrag);
+          // if(fvnBoxFeature.detectDevice()){
+          //   scrollContent.addClass('fvnHideCord');
+          // }else{            
+          //   scrollContent.removeClass('fvnHideCord');
+          //   scrollContent.append(scroller);      
+          // }
+          scrollContent.append(scroller);      
+          // window.addEventListener('mouseup',stopDrag);
+          // window.addEventListener('mousemove',scrollBar);
+          $(window).on("mousedown",function(e){
+            startDrag(e);
+          });
+          $(window).on("mousemove",function(e){
+            scrollBar(e);
+          });
+          $(window).on("mouseup",function(e){            
+            stopDrag(e);
+          });
         }
       }
       createScroll();
@@ -778,7 +798,6 @@ $(function($) {
       if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) ||
         /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) isMobile = true;
       else if(is_firefox && is_android){isMobile = true;} 
-      console.log(isMobile);
       return isMobile;
     },
     setResizeImg: function(fn_Opt) {      
